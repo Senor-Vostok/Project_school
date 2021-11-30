87599c19bdccff609bba80284b8458a6350c4288
Senor-Vostok <75014200+Senor-Vostok@users.noreply.github.com>
2021-11-30 19:14:41 +0500

    подготовка

--- main.py
+++ main.py
@@ -21,18 +21,28 @@ class Project(QWidget):
         self.button = QPushButton('Нажми', self)
         self.button.move(750, 450)
         self.button.setFixedSize(100, 30)
-        self.button.clicked.connect(self.prr)
+        self.button.clicked.connect(self.check)
         self.button.setStyleSheet('background: rgb(255,204,0);')
+
+        self.info = QLabel('Введите корректные данные пользователя!', self)
+        self.info.setStyleSheet('background: rgb(255,0,0);')
+        self.info.setFont(QFont('Arial', 10))
+        self.info.move(50, 850)
+        self.info.hide()
 
         self.win = QLineEdit(self)
         self.win.setFixedSize(200, 30)
         self.win.move(700, 400)
 
 
-    def prr(self):
-        self.test.setText('Успешно')
-        self.test.setFont(QFont('Arial', 72))
-        self.win.setStyleSheet('background: rgb(200, 100, 100);')
+    def check(self):
+        self.text = self.win.text()
+        if not self.text:
+            self.info.show()
+            self.win.setStyleSheet('background: rgb(255, 200, 200);')
+        else:
+            self.info.hide()
+            self.win.setStyleSheet('background: rgb(200, 255, 200);')
 
 
 if __name__ == '__main__':
