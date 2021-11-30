4136cd0754f448a96fcb8bc8f2ad5d7d3d8ad83e
Senor-Vostok <75014200+Senor-Vostok@users.noreply.github.com>
2021-11-30 16:53:22 +0500

    Первый файл-пролверка

---
+++ main.py
@@ -0,0 +1,24 @@
+from PyQt5.QtWidgets import QLabel, QPushButton, QWidget, QApplication
+from PyQt5.QtGui import QPixmap, QFont, QIcon, QFontDatabase, QCursor, QCloseEvent
+from PyQt5.QtCore import QSize, QTimer
+from PyQt5.Qt import Qt
+import sys
+
+class Project(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Project')
+        self.setFixedSize(1600, 900)
+        self.test = QLabel('Проверка заданий', self)
+        self.test.move(100, 100)
+        self.test.setFont(QFont('Arial', 72))
+
+
+if __name__ == '__main__':  # запуск всего!
+    app = QApplication(sys.argv)
+    progarmm = Project()
+    progarmm.show()
+    sys.exit(app.exec())
