e5e5075bde864c057e2acc6b29552626aa863cf3
Senor-Vostok <75014200+Senor-Vostok@users.noreply.github.com>
2021-11-30 18:36:17 +0500

    Задатки регистрации

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from PyQt5.QtWidgets import QLabel, QPushButton, QWidget, QApplication
+from PyQt5.QtWidgets import QLabel, QPushButton, QWidget, QApplication, QInputDialog, QLineEdit
 from PyQt5.QtGui import QPixmap, QFont, QIcon, QFontDatabase, QCursor, QCloseEvent
 from PyQt5.QtCore import QSize, QTimer
 from PyQt5.Qt import Qt
 import sys
+
 
 class Project(QWidget):
     def __init__(self):
@@ -12,12 +13,27 @@ class Project(QWidget):
     def initUI(self):
         self.setWindowTitle('Project')
         self.setFixedSize(1600, 900)
-        self.test = QLabel('Проверка', self)
+
+        self.test = QLabel('Регистрация/вход', self)
         self.test.move(100, 100)
         self.test.setFont(QFont('Arial', 72))
 
+        self.button = QPushButton('Нажми', self)
+        self.button.move(750, 450)
+        self.button.setFixedSize(100, 50)
+        self.button.clicked.connect(self.prr)
 
-if __name__ == '__main__':  # запуск всего!
+        self.win = QLineEdit(self)
+        self.win.setFixedSize(200, 30)
+        self.win.move(700, 400)
+
+
+    def prr(self):
+        self.test.setText('Успешно')
+        self.test.setFont(QFont('Arial', 72))
+
+
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     progarmm = Project()
     progarmm.show()
